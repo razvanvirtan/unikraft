84aecdd3ee94b9a6e1a162a4ab2af90596623b4a
Razvan Virtan <virtanrazvan@gmail.com>
2023-06-18 22:08:34 +0300

    lib/uklock: Implement spinning rwlocks

--- lib/uklock/rwlock.c
+++ lib/uklock/rwlock.c
@@ -33,31 +33,14 @@ void uk_rwlock_rlock(struct uk_rwlock *rwl)
 
 	uk_spin_lock(&rwl->sl);
 	rwl->npending_reads++;
+	uk_spin_unlock(&rwl->sl);
 
-	/* We let readers wait when there are writers pending. This is
-	 * necessary to avoid a situation where new readers continuously enter
-	 * the critical section while other readers are still in - thereby
-	 * starving the writers. However, at the same time we must not starve
-	 * readers when there are writers. Thus we always wake up readers when
-	 * a writer unlocks. The first wait() functions as a queue where we
-	 * enqueue readers that arrive while a writer had the lock (or is going
-	 * to get the lock next). When the writer unblock the readers, they
-	 * fall to the second wait() and eventually can enter. New readers,
-	 * however, block again on the first wait(), thus giving priority to
-	 * waiting writers.
-	 */
-	if (rwl->npending_writes > 0)
-		uk_waitq_wait_locked(&rwl->shared,
-				     uk_spin_lock,
-				     uk_spin_unlock,
-				     &rwl->sl);
-
-	/* Wait in case a writer acquired the lock (in the meantime) */
-	uk_waitq_wait_event_locked(&rwl->shared,
-				   rwl->nactive >= 0,
-				   uk_spin_lock,
-				   uk_spin_unlock,
-				   &rwl->sl);
+	while (1) {
+		uk_spin_lock(&rwl->sl);
+		if (rwl->nactive >= 0)
+			break;
+		uk_spin_unlock(&rwl->sl);
+	}
 
 	rwl->nactive++;
 	rwl->npending_reads--;
@@ -71,14 +54,13 @@ void uk_rwlock_wlock(struct uk_rwlock *rwl)
 	uk_spin_lock(&rwl->sl);
 	rwl->npending_writes++;
 
-	/* Wait for all readers to have left the lock. New readers will
-	 * block in uk_rwlock_rlock while we are waiting.
-	 */
-	uk_waitq_wait_event_locked(&rwl->exclusive,
-				   rwl->nactive == 0,
-				   uk_spin_lock,
-				   uk_spin_unlock,
-				   &rwl->sl);
+	uk_spin_unlock(&rwl->sl);
+	while (1) {
+		uk_spin_lock(&rwl->sl);
+		if (rwl->nactive == 0)
+			break;
+		uk_spin_unlock(&rwl->sl);
+	}
 
 	UK_ASSERT(rwl->npending_writes > 0);
 	UK_ASSERT(rwl->nactive == 0);
@@ -105,9 +87,6 @@ void uk_rwlock_runlock(struct uk_rwlock *rwl)
 	rwl->nactive--;
 	wake_writers = (rwl->nactive == 0 && rwl->npending_writes > 0);
 	uk_spin_unlock(&rwl->sl);
-
-	if (wake_writers)
-		uk_waitq_wake_up_one(&rwl->exclusive);
 }
 
 void uk_rwlock_wunlock(struct uk_rwlock *rwl)
@@ -126,11 +105,6 @@ void uk_rwlock_wunlock(struct uk_rwlock *rwl)
 	rwl->nactive = 0;
 	wake_readers = (rwl->npending_reads > 0);
 	uk_spin_unlock(&rwl->sl);
-
-	if (wake_readers)
-		uk_waitq_wake_up(&rwl->shared);
-	else
-		uk_waitq_wake_up_one(&rwl->exclusive);
 }
 
 void uk_rwlock_upgrade(struct uk_rwlock *rwl)
@@ -151,12 +125,14 @@ void uk_rwlock_upgrade(struct uk_rwlock *rwl)
 		 */
 		rwl->npending_writes++;
 		rwl->nactive--;
-
-		uk_waitq_wait_event_locked(&rwl->shared,
-					   rwl->nactive == 0,
-					   uk_spin_lock,
-					   uk_spin_unlock,
-					   &rwl->sl);
+		
+		uk_spin_unlock(&rwl->sl);
+		while (1) {
+			uk_spin_lock(&rwl->sl);
+			if (rwl->nactive == 0)
+				break;
+			uk_spin_unlock(&rwl->sl);
+		}
 
 		UK_ASSERT(rwl->npending_writes > 0);
 		UK_ASSERT(rwl->nactive == 0);
@@ -186,7 +162,4 @@ void uk_rwlock_downgrade(struct uk_rwlock *rwl)
 	rwl->nactive = 1;
 	wake_readers = (rwl->npending_reads > 0);
 	uk_spin_unlock(&rwl->sl);
-
-	if (wake_readers)
-		uk_waitq_wake_up(&rwl->shared);
 }
